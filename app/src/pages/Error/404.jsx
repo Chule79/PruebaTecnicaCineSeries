19b036ab72f0449042b20a7912fa24899909cb9e
Chule79 <jsantoslopez99@gmail.com>
2022-11-06 12:18:17 +0100

    final 3

--- app/src/pages/Error/404.jsx
+++ app/src/pages/Error/404.jsx
@@ -3,7 +3,6 @@ import Pagina from '../../components/Pagina/Pagina';
 import "./404.css"
 const Error404 = () => {
   return <div className="errorScreen">
-  <Pagina/>
   Oops, something went wwrong...
   </div>;
 };
